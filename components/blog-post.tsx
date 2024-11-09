56034737dcccf70fd6b3e4ea644237e6609044a2
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-11-08 19:50:52 -0500

    Update blog post and project card components

--- components/blog-post.tsx
+++ components/blog-post.tsx
@@ -1,19 +1,18 @@
+import Link from "next/link";
+
 export function BlogPost({ blogPost }: any) {
   return (
     <div>
-      <div className="text-[26px] md:text-[30px]">{blogPost.title}</div>
-      <div className="text-[16px] md:text-[20px] flex items-center">
-        <div>{blogPost.date}</div>
-        <div className="px-[4px]">|</div>
-        <div className="text-[#8695A4]">{blogPost.tags}</div>
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        quae quos rerum ex, distinctio quia consequuntur eveniet ullam provident
-        alias reprehenderit quasi neque ea error id dolor. Nulla, sapiente
-        provident!
-      </div>
-      <hr />
+      <Link href={blogPost.blogUrl}>
+        <div className="text-[26px] md:text-[30px]">{blogPost.title}</div>
+        <div className="text-[14px] md:text-[16px] flex items-center">
+          <div>{blogPost.date}</div>
+          <div className="px-[10px]">|</div>
+          <div className="text-[#8695A4]">{blogPost.tags}</div>
+        </div>
+        <div className="line-clamp-3">{blogPost.description}</div>
+        <hr className="my-[16px]" />
+      </Link>
     </div>
   );
 }
--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,26 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProjectCard({ project }: any) {
   return (
     <div className="md:flex md:items-start mb-[30px] md:mb-[10px]">
       <div className="flex-none md:pt-[10px] md:mr-[14px]">
-        <Image
-          src={project.imageSrc}
-          alt={project.title}
-          width={338}
-          height={230}
-        />
+        <Link href={project.projectUrl} className="w-full">
+          <Image
+            src={project.imageSrc}
+            alt={project.title}
+            width={338}
+            height={230}
+          />
+        </Link>
       </div>
       <div className="max-w-[338px] md:max-w-[680px]">
-        <div className="text-[18px] md:text-[22px]">{project.title}</div>
-        <div className="text-[12px] md:text-[14px] text-white">
-          <span className="bg-[#142850] py-[2px] px-[8px] rounded-xl">
-            {project.startDate}
-          </span>
-        </div>
-        <div className="text-[16px] mt-[4px] line-clamp-2 md:line-clamp-4">
-          {project.description}
-        </div>
+        <Link href={project.projectUrl} className="w-full">
+          <div className="text-[18px] md:text-[22px]">{project.title}</div>
+          <div className="text-[12px] md:text-[14px] text-white my-[6px]">
+            <span className="bg-[#142850] py-[2px] px-[8px] rounded-xl">
+              {project.startDate}
+            </span>
+          </div>
+          <div className="text-[16px] mt-[4px] line-clamp-2 md:line-clamp-4">
+            {project.description}
+          </div>
+        </Link>
       </div>
     </div>
   );
